a258697863b63a50c9b1897ffbc3c5dffceb5115
Kamil Musiał <kmusial40@gmail.com>
2020-07-03 21:48:16 +0200

    Create Skeletron for NavComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,97 @@
 import React from 'react';
+import NavComponent from "./components/Nav";
+import ASRoomLogo from './img/ASRoomLogo.png';
 
 class App extends React.Component<any, any> {
+
+    state = {
+        isPL: true,
+        PL: {
+            Nav: {
+                mainLink: [
+                    {
+                        href: "/",
+                        text: "O nas"
+                    },
+                    {
+                        href: "/",
+                        text: "Odnawialne Zrodla Energii"
+                    },
+                    {
+                        href: "/",
+                        text: "Systemy Oczyszczania Sciekow"
+                    },
+                    {
+                        href: "/",
+                        text: "Modernizacja"
+                    },
+                    {
+                        href: "/",
+                        text: "Panel Handlowy"
+                    }
+                ],
+                Logo: {
+                    src: ASRoomLogo,
+                    alt: "www.asroom.pl - zdjecie loga firmy ASRoom w formacie .png",
+                },
+                pageLink: [
+                    {
+                        href: "/",
+                        text: "O nas"
+                    },
+                    {
+                        href: "/",
+                        text: "Kontakt"
+                    }
+                ]
+            }
+        },
+        EN: {
+            Nav: {
+                mainLink: [
+                    {
+                        href: "/",
+                        text: "About Us"
+                    },
+                    {
+                        href: "/",
+                        text: "Renewable Energy Sources"
+                    },
+                    {
+                        href: "/",
+                        text: "Sewage Treatment Systems"
+                    },
+                    {
+                        href: "/",
+                        text: "Modernization"
+                    },
+                    {
+                        href: "/",
+                        text: "Trade Panel"
+                    }
+                ],
+                Logo: {
+                    src: ASRoomLogo,
+                    alt: "www.asroom.pl - photo of the ASRoom company logo in .png format",
+                },
+                pageLink: [
+                    {
+                        href: "/",
+                        text: "About Us"
+                    },
+                    {
+                        href: "/",
+                        text: "Contact"
+                    }
+                ]
+            }
+        }
+    }
+
     render() {
         return (
             <div className="App">
-
+                <NavComponent Nav={this.state.isPL ? this.state.PL.Nav : this.state.EN.Nav}/>
             </div>
         );
     }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,109 @@
 import React from "react";
+import {BrowserRouter as Router, Link} from "react-router-dom";
 
-export default class NavComponent extends React.Component<any, any> {
+interface Props {
+    Nav: {
+        mainLink: {
+            href: string,
+            text: string
+        }[],
+        Logo: {
+            src: string,
+            alt: string,
+        },
+        pageLink: {
+            href: string,
+            text: string
+        }[]
+    }
+}
+
+export default class NavComponent extends React.Component<Props, any> {
     render() {
-        return (
-            <div>
 
-            </div>
+        const {mainLink, Logo, pageLink} = this.props.Nav;
+
+        return (
+            <Router>
+                <div className={"navigation"}>
+                    <div className={"navigation__aside"}>
+                        <nav className={"navigation__aside__box"}>
+                            <ul className={"navigation__aside__box__navHolder"}>
+                                {mainLink.map((value, index) => (
+                                    <NavComponentMainLink
+                                        key={index}
+                                        href={value.href}
+                                        text={value.text}
+                                    />
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className={"navigation__topPanel"}>
+                        <div className={"navigation__topPanel__iconHolder"}>
+                            <div className={"navigation__topPanel__iconHolder__burger"}>
+                                <span/>
+                                <span/>
+                                <span/>
+                            </div>
+                            <img
+                                className={"navigation__topPanel__iconHolder__image"}
+                                src={Logo.src}
+                                alt={Logo.alt}
+                            />
+                        </div>
+                        <div className={"navigation__topPanel__pageNavigation"}>
+                            {pageLink.map((value, index) => (
+                                <NavComponentPageLink
+                                    key={index}
+                                    text={value.text}
+                                    href={value.href}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}
+
+interface Props3 {
+    href: string;
+    text: string;
+}
+
+export class NavComponentPageLink extends React.Component<Props3, any> {
+    render() {
+        const {href, text} = this.props;
+        return (
+            <Link
+                to={href}
+                className={"navigation__topPanel__pageNavigation__link"}
+            >
+                {text}
+            </Link>
+        );
+    }
+}
+
+interface Props2 {
+    href: string;
+    text: string;
+}
+
+export class NavComponentMainLink extends React.Component<Props2, any> {
+    render() {
+        const {text, href} = this.props;
+        return (
+            <li className={"navigation__aside__box__navHolder__box"}>
+                <Link
+                    className={"navigation__aside__box__navHolder__box__item"}
+                    to={`${href}`}
+                >
+                    {text}
+                </Link>
+            </li>
         );
     }
 }
